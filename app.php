2ae8bb34192d8d6b714c5ab6101cb47ae8e4e664
Jakob Pupke <jakob.pupke@gmail.com>
2015-03-27 15:15:28 +0100

    Change to mysqli

--- app.php
+++ app.php
@@ -14,24 +14,28 @@ $app->get('/', function () use ($app) {
 
 $app->get('/within', function (Request $request) use ($app) {
 
-	$con = mysql_connect(getenv('db_host'),getenv('db_user'), getenv('db_pw')) or die('Could not connect: ' . mysql_error());
+	$mysqli = new mysqli(getenv('db_host'),getenv('db_user'), getenv('db_pw'), "airports");
 
- 	mysql_select_db("airports", $con);
+	if ($mysqli->connect_errno) {
+	    printf("Connect failed: %s\n", $mysqli->connect_error);
+	    exit();
+	}
 
- 
  	$query = 'SELECT * FROM tbl_airp WHERE (latitude_deg BETWEEN ' .
  		$request->get('lat1') . ' AND ' . $request->get('lat2') . ') AND (longitude_deg BETWEEN ' . 
  		$request->get('lon1') . ' AND ' . $request->get('lon2') .') AND type = "large_airport"';
 
- 	$result = mysql_query($query, $con) or die (mysql_error());
+	$rows = array();
 
- 	$rows = array();
-	while($r = mysql_fetch_assoc($result)) {
-	    $rows[] = $r;
-	}
+ 	if($result = $mysqli->query($query)) {
+		while($r = $result->fetch_assoc()) {
+		    $rows[] = $r;
+		}
+	 	
+	 	$mysqli->close();
+ 	}
+
  	
- 	mysql_close($con);
-
  	return $app->json(json_encode($rows));
 
 });
